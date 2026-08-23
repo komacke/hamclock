075cc3b16afbeb5dc08e1ff09af900dfb7dc7399
Dave Koberstein <davek@komacke.com>
2026-08-23 15:13:36 -0700

    debug build signing
    Eventually we'll have to figure out the official release signing.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -117,12 +117,14 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
     }
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
