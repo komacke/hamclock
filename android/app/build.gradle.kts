bb16284ee18b1d326aa1c960982c1174df4ba793
Dave Koberstein <davek@komacke.com>
2026-08-25 09:43:47 -0700

    Bump to recommended jdk

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -229,12 +229,12 @@ fun promptPlain(prompt: String): String? {
 
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 
     externalNativeBuild {
